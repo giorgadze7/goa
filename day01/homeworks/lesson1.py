902a9d00da4d193f4fca6404dc3e5490653991b1
giorgadze7 <nikagiogadze379@gmail.com>
2024-09-14 12:11:19 +0400

    second commit

---
+++ day01/homeworks/lesson1.py
@@ -0,0 +1,41 @@
+name = "nika"
+#name არის ცვლადი
+# = არის ცვლდისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#  "nika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "keshelava"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ან სტრინგი
+
+name = "nika" #ეს არის str (string) ტიპის ცვლადი
+age = 22 # ეს არის int ( ingeger ) მთელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი ( ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_propgramming = True  #True ან False
+is_ugly = False     #snakecase(  უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False  # ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლო
+# print(name + age)
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
+
+
+
+
+
+
+
---
+++ day02/lesson1.py
@@ -0,0 +1,21 @@
+name = "nika"
+surname = "giorgadze"
+age = 12
+live = "georgia"  
+year = 2012
+month = "april"
+day = 7
+height =152
+season = 'spring'
+
+print( "my name is " + name + ",my surname is " + surname + " ,my age - " + str(age) + ", i live in " + live + ", my height is " + str(height) +'cm' + ", i was born at " + str(year) + " on " + season + " on " + str(day) + " in " + month)
+
+
+
+
+
+# my name is nika, my surname is giorgadze, my age - 12, i live in georgia, i born on spring, my height is 152cm
+# print(2+2) #4
+# print("2" + "2")  #22
+
+
