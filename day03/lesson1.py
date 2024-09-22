68eb0afef807b3ef863360a74f921f749f75674c
giorgadze7 <nikagiogadze379@gmail.com>
2024-09-22 22:46:46 +0400

    'Day3'

---
+++ day03/lesson1.py
@@ -0,0 +1,54 @@
+num1 = 16
+num2 = 21
+num3 = 7
+num4 = 32
+num5 = 12
+num6 = 19
+num7 = 34
+num8 = 37
+num9 = 4
+num10 = 20
+
+print(num1 + num2)
+
+num11 = 2.4
+num12 = 3.5
+num13 = 1.7
+num14 = 3.2
+num15 = 4.3
+num16 = 6.2
+num17 = 1.1
+num18 = 2.9
+num19 = 3.7
+num20 = 4.8
+
+fruit1 = "11" + " carambola "
+fruit2 = "16" + " blackberry "
+fruit3 = "19" + " physalis "
+fruit4 = "24" + " grapes "
+fruit5 = "8" + " pomegranate "
+fruit6 = "20" + " strawberry "
+fruit7 = "15" + " orange "
+fruit8 = "21" + " cherry "
+fruit9 = "9" + " pear "
+fruit10 = "10" + " coconut "
+fruit11 = "12" + " rambutan "
+fruit12 = "22" + " dragon fruit "
+fruit13 = "8" + " pineapple"
+fruit14 = "25" + " mango "
+fruit15 = "23" + " papya "
+fruit16 = "2" + " watermelon "
+fruit17 = "5" + " fig "
+fruit18 = "6" + " kiwi "
+fruit19 = "31" + " lime "
+fruit20 = "7" + " guava "
+
+age = 12
+weight =39.8
+height = 152
+
+film1 = "Titanic"
+film2 = "jurassic park"
+ 
+print(film1 + " " + film2)
+
