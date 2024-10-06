ec0d6ab5de3f04f18e5d763766cacda9a6606aac
giorgadze7 <nikagiogadze379@gmail.com>
2024-10-06 19:58:23 +0400

    day05

---
+++ day05/lesson6.py
@@ -0,0 +1,48 @@
+name = "nika" 
+surname = "giorgadze" 
+age = 12
+height = 152
+weight = 38.9
+year = 2012
+month = "april" 
+day = 7
+country = "georgia"
+city = "tbilisi"
+place = "isani"
+animal = "dog"
+fruit = "apple" 
+candy = "kitkat"
+school = 87
+sport = "swim" 
+game = "roblox"
+roblox = "mm2"
+film = "poseidon"
+anime = "my hero academia"
+
+name1 = "GOA"
+name2 = "BEST"
+print(name1 + " " + name2)
+
+aura = 30
+print(aura + 30)
+print(aura - 10)
+print(aura / 3)
+print(aura * 5)
+
+name = "Nika"
+score = 30
+
+print(name)
+print(score)
+
+flight_n = "BA0117"
+# ვაკეთებთ ცვლადს რომელსაც ჩვენ დავაწერთ flight_n ეს არის ცვლადის სახელი, ხოლო "BA0117" ეს არის ცვლადის მნიშვნელობა. string ტიპისაა
+destination = "New York"
+# ვაკეთებთ ცვლადს რომლესაც ჩვენ დავაწერთ destination(ანუ დანიშნულება) ეს არის ცვლადის სახელი შემდეგ ტოლობას და მერე New York-ს ეს არის ცვლადის მნიშვნელობა. string ტიპისაა
+distance = 1580
+# ვაკეთებთ ცვლადს რომელსაც დავაწერთ distance(ანუ მანძილი) ეს არის ცვლადის სახელი შემდეგ ტოლობას და მერე 1580 ეს არის ცვლადის მნიშვნელობა. integer ტიპისაა
+
+print(flight_n)
+print(destination)
+print(distance)
+# print-ით ჩვენ გამოგვაქვს flight_n,destination,distance ცვლადები ტერმინალში
