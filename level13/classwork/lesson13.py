22a388193ce13ef79b75578860d6b5661dfa57c4
giorgadze7 <nikagiogadze379@gmail.com>
2024-11-30 16:28:45 +0400

    level12

---
+++ level13/classwork/lesson13.py
@@ -0,0 +1,24 @@
+print(3 < 5 and 4 < 5)
+print(3 < 5 and 4 > 5)
+print(3 > 5 and 4 < 5)
+print(3 > 5 and 4 > 5)
+print(4 < 8 and 9 < 10)
+print(4 < 8 and 9 > 10)
+print(4 > 8 and 9 < 10)
+print(4 > 8 and 9 > 10)
+print(5 < 7 and 10 > 5)
+print(5 < 7 and 10 < 5)
+print(3 < 5 or 4 < 5)
+print(3 < 5 or 4 > 5)
+print(3 > 5 or 4 < 5)
+print(3 > 5 or 4 > 5)
+print(4 < 8 or 9 < 10)
+print(4 < 8 or 9 > 10)
+print(4 > 8 or 9 < 10)
+print(4 > 8 or 9 > 10)
+print(5 < 7 or 10 > 5)
+print(5 < 7 or 10 < 5)
+
+
+
+
